d80e0b4dd3504a4956f5dc8e7fd53e747341c132
Ramiro <ramirods93@hotmail.com>
2025-06-01 17:47:47 -0300

    changed script for dispatching monitor

--- .config/hypr/dispatch_monitors.sh
+++ .config/hypr/dispatch_monitors.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 # dispatch monitor for laptop
-if [[ hostname == "*-lt" ]]
+if [[ $HOSTNAME == *-lt ]]
 then
-  hyprctl keyword monitor "auto,auto,0x0,1"
+  hyprctl keyword monitor "eDP-1,preferred,auto,1"
 else
   hyprctl keyword monitor "HDMI-A-1,1920x1080,-1920x0,1" &&\
   hyprctl keyword monitor "DP-1,1920x1080,0x0,1" &&\
