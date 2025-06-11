1ff761767f5d13e2bf77bbb2b6180360749e6832
Ramiro <ramirods93@hotmail.com>
2025-06-10 22:02:37 -0300

    added telescope config
    added .git to the ignored list of telescope, currently doens't work.
    added vim emote to waybar.
    added hypr window rule for proton vpn, currently doesn't work.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -32,7 +32,7 @@ return {
         local builtin = require('telescope.builtin')
         local utils = require("telescope.utils")
         vim.keymap.set('n', '<leader>pf', function()
-          builtin.find_files({ hidden = true })
+          builtin.find_files({ hidden = true, file_ignore_patterns = { "^./.git/" } })
         end, { desc = "Find Files" })
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>ps', function()
