4bf71c8fabf0d1fa2a88501e6b052b3cf524532d
Ramiro <ramirods93@gmail.com>
2025-12-13 15:50:34 -0300

    added space

--- config/hypr/dispatch_monitors.sh
+++ config/hypr/dispatch_monitors.sh
@@ -5,8 +5,8 @@ if [[ $HOSTNAME == *-lt ]]
 then
   hyprctl keyword monitor "eDP-1,preferred,auto,1"
 else
-  hyprctl keyword monitor "HDMI-A-1,1920x1080,-1920x0,1" &&\
-  hyprctl keyword monitor "DP-1,1920x1080,0x0,1" &&\
+  hyprctl keyword monitor "HDMI-A-1,1920x1080,-1920x0,1" && \
+  hyprctl keyword monitor "DP-1,1920x1080,0x0,1" && \
   hyprctl keyword monitor "DP-2,1920x1080,1920x0,1"
 fi
 
